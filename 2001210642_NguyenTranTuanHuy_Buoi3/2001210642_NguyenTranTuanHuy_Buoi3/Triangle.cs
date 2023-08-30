4d2cbb843b8f26d2c7cbdd1c403e19701086b725
A209PC49 <tuanhuy1104pro@gmail.com>
2023-08-30 13:56:06 +0700

    Edit ATriangle.css

--- 2001210642_NguyenTranTuanHuy_Buoi3/2001210642_NguyenTranTuanHuy_Buoi3/Triangle.cs
+++ 2001210642_NguyenTranTuanHuy_Buoi3/2001210642_NguyenTranTuanHuy_Buoi3/Triangle.cs
@@ -29,5 +29,21 @@ namespace _2001210642_NguyenTranTuanHuy_Buoi3
             get { return mc; }
             set { mc = value; }
         }
+        public Triangle() : this(0,0,0)
+        {
+
+        }
+        public Triangle(int ma , int mb, int mc)
+        {
+            
+            if(ma < 0 || mb < 0|| mc < 0 || (ma + mb <mc )
+            {
+                Ma = 0;
+                Mb = 0;
+                Mc = 0;
+            }
+        }
+       
+
     }
 }
