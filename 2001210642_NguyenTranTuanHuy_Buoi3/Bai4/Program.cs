bf897538200b5ae6addf395adaeecdac44dc3e61
Your Name <you@example.com>
2023-08-30 15:41:30 +0700

    Bai 3 and 4 Completed

---
+++ 2001210642_NguyenTranTuanHuy_Buoi3/Bai4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle Person1= new Vehicle("Huy","Honda",50,100);
+            Person1.tostring();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2001210642_NguyenTranTuanHuy_Buoi3/Bai4/Vehicle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+     public class Vehicle
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string Tag;
+
+        public string Tag1
+        {
+            get { return Tag; }
+            set { Tag = value; }
+        }
+        private int CC;
+
+        public int CC1
+        {
+            get { return CC; }
+            set { CC = value; }
+        }
+        private float giatri;
+
+        public float Giatri
+        {
+            get { return giatri; }
+            set { giatri = value; }
+        }
+
+        private float thue;
+
+        public float Thue
+        {
+            get { return thue; }
+            set {
+                if (value > 0)
+                    thue = value;
+            }
+        }
+         
+        public float tinhThue()
+        {
+            if (CC1 < 100)
+                thue = (float)(giatri * 1) / 100;
+
+            else
+            {
+                if(CC1 <= 200)
+                    thue = (float)(giatri * 3) / 100;
+                else
+                {
+                    thue = (float)(giatri * 5) / 100;
+                }
+            }
+
+            return thue;
+        }
+        public Vehicle(string name, string tag,int cc,float giatri)
+        {
+            this.name = name;
+            this.Tag = tag;
+            this.CC = cc;
+            this.giatri = giatri;
+            this.thue = tinhThue();
+        }
+         public void tostring()
+        {
+            Console.WriteLine("TenChuxe\t\tLoaixe\t\tDungtich\t\tTrigia\t\tThue");
+            Console.WriteLine("{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}",name,Tag,CC,giatri,thue);
+        }
+    }
+}
