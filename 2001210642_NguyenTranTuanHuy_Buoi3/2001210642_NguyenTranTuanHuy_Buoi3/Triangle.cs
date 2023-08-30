c488e107284565bc993bb8c7e989b91596f964db
A209PC49 <tuanhuy1104pro@gmail.com>
2023-08-30 13:32:25 +0700

    fist add

---
+++ 2001210642_NguyenTranTuanHuy_Buoi3/2001210642_NguyenTranTuanHuy_Buoi3/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2001210642_NguyenTranTuanHuy_Buoi3
+{
+    public class Triangle 
+    {
+        private int ma;
+
+        public int Ma
+        {
+            get { return ma; }
+            set { ma = value; }
+        }
+        private int mb;
+
+        public int Mb
+        {
+            get { return mb; }
+            set { mb = value; }
+        }
+        private int mc;
+
+        public int Mc
+        {
+            get { return mc; }
+            set { mc = value; }
+        }
+    }
+}
