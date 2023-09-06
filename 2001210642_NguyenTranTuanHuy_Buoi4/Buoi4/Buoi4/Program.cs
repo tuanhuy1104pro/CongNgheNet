435f22afdc5980274d3f6e33c97615d45d9a2901
A209PC50 <tuanhuy1104pro@gmail.com>
2023-09-06 13:52:22 +0700

    Bai7_Module0 Edited

--- 2001210642_NguyenTranTuanHuy_Buoi4/Buoi4/Buoi4/Program.cs
+++ 2001210642_NguyenTranTuanHuy_Buoi4/Buoi4/Buoi4/Program.cs
@@ -10,11 +10,17 @@ namespace Bai7_Module0
     {
         static void Main(string[] args)
         {
+            /// This part is for Declare a variable and call
             HinhTron hinh1 = new HinhTron();
             hinh1.Bankinh1 = 4;
-            Console.WriteLine(hinh1.Tam);
-            hinh1.TinhChuViTron();
-            hinh1.TinhDienTichTron();
+
+            ToaDo x = new ToaDo(5,5);
+            HinhTron hinh2 = new HinhTron(x,4);
+            
+            //////////// Excuted code and run example
+            Console.WriteLine(hinh2.Tam);
+            hinh2.TinhChuViTron();
+            hinh2.TinhDienTichTron();
             Console.ReadKey();
         }
     }
